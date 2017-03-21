22954d452920fa1ec29699ed7b716ad95a79a48c
fernandoporazzi <fernandoporazzi@gmail.com>
2017-03-21 14:58:05 -0300

    Add stylus and nib

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
+app.use(serveStatic(path.join(__dirname, 'app/public/dist/')));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'pug');
