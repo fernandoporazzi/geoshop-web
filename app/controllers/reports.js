99644d4308925093aba72ea23f3f2beae537da52
fernandoporazzi <fernandoporazzi@gmail.com>
2017-11-09 14:29:08 -0200

    Refine search when querying by last page

--- app/controllers/reports.js
+++ app/controllers/reports.js
@@ -2,6 +2,21 @@
 const SessionModel = require('../models/session.js');
 const statesAndCities = require('../data/statesAndCities.json');
 
+function filterPages(docs) {
+  let pages = [];
+  for (let i = 0; i < docs.length; i++) {
+    for (let j = 0; j < docs[i].navigation.length; j++) {
+      if (docs[i].navigation[j] != null) {
+        let part = docs[i].navigation[j].split(' ')[0];
+        if (!pages.includes(part)) {
+          pages.push(part);
+        }
+      }
+    }
+  }
+  return pages.sort();
+}
+
 function buildQuery(req) {
   let query = {};
   
@@ -37,12 +52,17 @@ function buildQuery(req) {
 module.exports = {
 
   index: (req, res, next) => {
-    res.render('reports', {
-      title: 'GeoShop - Relatórios',
-      storeName: req.user.storeName,
-      storeId: req.user._id,
-      isAuthenticated: req.isAuthenticated(),
-      states: statesAndCities.estados
+    SessionModel.find({}, 'navigation',(err, docs) => {
+      let pages = filterPages(docs);
+
+      res.render('reports', {
+        title: 'GeoShop - Relatórios',
+        storeName: req.user.storeName,
+        storeId: req.user._id,
+        isAuthenticated: req.isAuthenticated(),
+        states: statesAndCities.estados,
+        pages: pages
+      });
     });
   },
 
@@ -61,14 +81,33 @@ module.exports = {
   getData: (req, res, next) => {
     SessionModel.find(buildQuery(req.query), 
     (err, docs) => {
-      res.render('reports', {
-        title: 'GeoShop - Relatórios',
-        storeName: req.user.storeName,
-        storeId: req.user._id,
-        isAuthenticated: req.isAuthenticated(),
-        states: statesAndCities.estados,
-        sessions: docs
-      })
+
+      if (req.query.lastpage != '') {
+        let refine = [];
+        for (let i = 0; i < docs.length; i++) {
+          let nav = docs[i].navigation;
+          let lastItem = nav[nav.length - 1];
+
+          if (lastItem && lastItem.includes(req.query.lastpage)) {
+            refine.push(docs[i]);
+          }
+        }
+        docs = refine;
+      }
+
+      SessionModel.find({}, 'navigation',(err, navdocs) => {
+        let pages = filterPages(navdocs);
+      
+        res.render('reports', {
+          title: 'GeoShop - Relatórios',
+          storeName: req.user.storeName,
+          storeId: req.user._id,
+          isAuthenticated: req.isAuthenticated(),
+          states: statesAndCities.estados,
+          pages: pages,
+          sessions: docs
+        })
+      });
     })
   }
 };
